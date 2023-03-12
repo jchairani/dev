91170b9409e425330bf1ce51052b1e4c4c1fa690
DuySE <duynse@gmail.com>
2023-03-12 03:26:35 -0700

    feat: edit baseURL for axios

--- client/src/utils/api_instance.js
+++ client/src/utils/api_instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://bicdevs.herokuapp.com',
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
